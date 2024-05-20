ab378113d141e10304eb65d893bacabbbcaf2a62
Alex <alexmogollon123@gmail.com>
2024-05-20 18:32:09 -0500

    a

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function incrementCount() {
     if (count < 100) {
         count++;
         countElement.textContent = count;
-        setTimeout(incrementCount, 50); // Adjust the delay between increments (in milliseconds)
+        setTimeout(incrementCount, 1); // Adjust the delay between increments (in milliseconds)
     } else {
         // Redirect to another HTML page when count reaches 100
         window.location.href = 'finale.html'; // Replace 'anotherpage.html' with the desired file name
