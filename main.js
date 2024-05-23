e7ebb20ba9860263550871aa0dc22d51ab677e3d
Alex <alexmogollon123@gmail.com>
2024-05-22 18:31:35 -0700

    d

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+window.onload = function() {
+const textElement = document.getElementById('rtext');
+let text = textElement.textContent;
+let index = 0;
+
+function removeLetter() {
+    if (index < text.length) {
+        textElement.textContent = text.slice(0, index) + text.slice(index + 1);
+        index++;
+        setTimeout(removeLetter, 200); // Adjust the interval as needed (200ms in this case)
+    }
+}
+
+removeLetter();
+};
+
+
+
+
+
+
 let count = 0;
 const countElement = document.querySelector('#count-number');
 
